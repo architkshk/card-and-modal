a03c40b98fd9fa03a9dc671352197bc59b2dceee
Archit Kaushik <asarchit65@gmail.com>
2018-01-27 23:20:59 +0530

    WIP: Modal

---
+++ src/components/Cards/CardModal.js
@@ -0,0 +1,13 @@
+import React,{ Component } from 'react';
+import './Cards.css';
+
+class CardModal extends Component {
+    render(){
+        return (
+            <div className="card-modal">
+            </div>
+        );
+    }
+}
+
+export default CardModal;
